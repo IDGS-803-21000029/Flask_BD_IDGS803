497921b2a03d5a3d7018683aa6a5b9507d649e86
Josue Ivan Gonzalez Cordova <ivan.glzcor.tech@outlook.com>
2024-02-23 18:36:10 -0600

    first commit

---
+++ forms.py
@@ -0,0 +1,29 @@
+
+from wtforms import Form
+from wtforms import StringField, EmailField, IntegerField
+from wtforms import validators
+
+class UsersForm(Form):
+    nombre = StringField('nombre',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa un nombre valido')
+    ])
+    apaterno = StringField('apaterno',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa un apellido valido')
+    ])
+    amaterno = StringField('amaterno',[
+        validators.DataRequired(message='El campo es requerido'),
+        validators.length(min=4, max=10, message='Ingresa un apellido valido')
+    ])
+    edad = IntegerField('edad',[
+        validators.number_range(min=1, max=20, message='Valor no valido')
+    ])
+    correo = EmailField('correo',[
+        validators.Email(message='Ingresa un correo valido')
+    ])
+
+
+    
+
+
---
+++ main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request, Response
+import forms
+from flask import flash
+from flask_wtf.csrf import CSRFProtect
+from flask import g
+from flask import redirect
+from config import DevelopmentConfig
+
+
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+csrf=CSRFProtect()
+
+app.secret_key='esta es la clave secreta'
+
+@app.errorhandler(404)
+def page_not_find(e):
+    return render_template('404.html'),404
+
+
+@app.route("/index")
+def index():
+    g.nombre = 'Daniel'
+    escuela = "UTL"
+    alumnos = ["Mario", "Pedro", "Luis", "Dario"]
+
+    return render_template("index.html", escuela=escuela, alumnos=alumnos)
+
+@app.route("/alumnos", methods=["GET", "POST"])
+def alumnos():
+    nom = ''
+    apa = ''
+    ama = ''
+    alum_form = forms.UsersForm(request.form)
+    if request.method == 'POST' and alum_form.validate():
+        nom = alum_form.nombre.data
+        apa = alum_form.apaterno.data
+        ama = alum_form.amaterno.data
+        edad = alum_form.edad.data
+        mensaje='Bienvenido {}'.format(nom)
+        flash(mensaje)
+
+        print("Nombre: {}".format(nom))
+        print("Apaterno: {}".format(apa))
+        print("Amaterno: {}".format(ama))
+
+    return render_template("alumnos.html", form=alum_form, nom=nom, apa=apa, ama=ama)
+
+if __name__ == "__main__":
+    csrf.init_app(app)
+    app.run()
